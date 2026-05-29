0f3a88e2a9f8f0992b66004e206ab2cd71bbe027
Harsh Arya <harsharya8946@gmail.com>
2026-05-29 22:44:52 +0530

    maintaining uniqueness

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,6 +6,23 @@ const authRouter = express.Router()
 authRouter.post('/register', async(req , res) => {
     const {name, contact, email, userid , password} = req.body
 
+    const isUseEmail = await loginModel.findOne({ email })
+
+    const isUserID = await loginModel.findOne({ userid })
+
+    if(isUseEmail){
+        return res.status(409).json({
+            'message':'Email already in use.'
+        })
+    }
+
+    if(isUserID){
+        return res.status(409).json({
+            'message':'User ID already created.'
+        })
+    }
+
+
    const registerUser =  await loginModel.create({ name, contact, email, userid, password})
 
    res.status(201).json({
